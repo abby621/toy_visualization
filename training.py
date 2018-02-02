73b383809f60e287f537737326761869b1ba27f6
abby <sw62190>
2018-02-02 11:31:40 -0600

    update op issue

--- training.py
+++ training.py
@@ -180,7 +180,7 @@ def main(margin,batch_size,output_size,learning_rate,is_overfitting,whichGPU):
     for step in range(num_iters):
         start_time = time.time()
         batch, labels, ims = train_data.getBatch()
-        _, loss_val, vv = sess.run([train_op, loss, varvar], feed_dict={image_batch: batch, label_batch: labels})
+        _, _, loss_val, vv = sess.run([train_op, update_ops, loss, varvar], feed_dict={image_batch: batch, label_batch: labels})
         end_time = time.time()
         duration = end_time-start_time
         out_str = 'Step %d: loss = %.6f (%.3f sec)' % (step, loss_val, duration)
