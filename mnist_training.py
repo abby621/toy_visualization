35e20b4648f9cc72520b0960fd198e0a487c9241
abby <sw62190>
2018-02-08 11:43:20 -0600

    batch norm decay = .9

--- mnist_training.py
+++ mnist_training.py
@@ -96,7 +96,7 @@ def main(margin,batch_size,output_size,learning_rate,is_overfitting,whichGPU):
         final_batch = tf.add(tf.subtract(image_batch,repMeanIm),noise)
 
     print("Preparing network...")
-    with slim.arg_scope(resnet_v2.resnet_arg_scope(batch_norm_decay=.997)):
+    with slim.arg_scope(resnet_v2.resnet_arg_scope(batch_norm_decay=.9)):
         _, layers = resnet_v2.resnet_v2_50(final_batch, num_classes=output_size, is_training=True, scope='resnet')
 
     featLayer = 'resnet/logits'
