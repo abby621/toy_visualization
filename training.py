66f26f01c8584abb336e29c8f6fd37607da05722
abby <sw62190>
2018-02-08 15:40:04 -0600

    back to resnet

--- training.py
+++ training.py
@@ -107,7 +107,7 @@ def main(margin,batch_size,output_size,learning_rate,is_overfitting,whichGPU):
         final_batch = tf.add(tf.subtract(image_batch,repMeanIm),noise)
 
     print("Preparing network...")
-    with slim.arg_scope(resnet_v2.resnet_arg_scope(is_training=True, updates_collections=None, batch_norm_decay=.5, fused=True)):
+    with slim.arg_scope(resnet_v2.resnet_arg_scope(is_training=True, updates_collections=None, batch_norm_decay=.9, fused=True)):
         _, layers = resnet_v2.resnet_v2_50(final_batch, num_classes=output_size, is_training=True, scope='resnet')
 
     featLayer = 'resnet/logits'
