aaf177f1c3ea56f19653978e2f5a00ac4296c46a
abby <sw62190>
2018-02-12 10:51:53 -0600

    use batch norm = false

--- training.py
+++ training.py
@@ -113,7 +113,7 @@ def main(margin,batch_size,output_size,learning_rate,is_overfitting,whichGPU):
     featLayer = 'resnet/logits'
     feat = tf.squeeze(tf.nn.l2_normalize(layers[featLayer],3))
     # feat = tf.squeeze(tf.nn.l2_normalize(tf.get_default_graph().get_tensor_by_name("pool5:0"),3))
-    varvar = tf.get_default_graph().get_tensor_by_name("resnet/block1/unit_1/bottleneck_v2/conv1/BatchNorm/moving_mean:0")
+    # varvar = tf.get_default_graph().get_tensor_by_name("resnet/block1/unit_1/bottleneck_v2/conv1/BatchNorm/moving_mean:0")
 
     expanded_a = tf.expand_dims(feat, 1)
     expanded_b = tf.expand_dims(feat, 0)
@@ -177,14 +177,13 @@ def main(margin,batch_size,output_size,learning_rate,is_overfitting,whichGPU):
     for step in range(num_iters):
         start_time = time.time()
         batch, labels, ims = train_data.getBatch()
-        _, _, vv, loss_val = sess.run([train_op, update_ops, varvar, loss], feed_dict={image_batch: batch, label_batch: labels})
+        _, _, loss_val = sess.run([train_op, update_ops, loss], feed_dict={image_batch: batch, label_batch: labels})
         end_time = time.time()
         duration = end_time-start_time
         out_str = 'Step %d: loss = %.6f (%.3f sec)' % (step, loss_val, duration)
         # print(out_str)
         if step % summary_iters == 0 or is_overfitting:
             print(out_str)
-            print vv[0:5]
             train_log_file.write(out_str+'\n')
         # Update the events file.
         # summary_str = sess.run(summary_op)
