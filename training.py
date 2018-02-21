9222d5f15a950dba7d55f29de1942f463558aa95
abby <sw62190>
2018-02-21 09:43:17 -0600

    is_training=true during training, use batch decay norm

--- training.py
+++ training.py
@@ -112,7 +112,7 @@ def main(margin,batch_size,output_size,learning_rate,is_overfitting,whichGPU,l1_
     # with slim.arg_scope(resnet_v2.resnet_arg_scope(is_training=False, use_batch_norm=True, updates_collections=None, batch_norm_decay=.9, fused=True)):
     #     _, layers = resnet_v2.resnet_v2_50(final_batch, use_batch_norm=True,num_classes=output_size, is_training=False)
     with slim.arg_scope(resnet_v2.resnet_arg_scope(updates_collections=None, batch_norm_decay=.9)):
-        _, layers = resnet_v2.resnet_v2_50(final_batch, num_classes=output_size, is_training=False)
+        _, layers = resnet_v2.resnet_v2_50(final_batch, num_classes=output_size, is_training=True)
 
     featLayer = 'resnet_v2_50/logits'
     feat = tf.squeeze(tf.nn.l2_normalize(layers[featLayer],3))
